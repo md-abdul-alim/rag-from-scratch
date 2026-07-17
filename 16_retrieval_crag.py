b78dee04a159665fa0b9e5af2e097e42e1cd811a
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-07-17 11:37:27 +0600

    Add initial implementation of retrieval_crag.py with references to YouTube and related papers

---
+++ 16_retrieval_crag.py
@@ -0,0 +1,10 @@
+
+
+"""
+Y.T.=https://www.youtube.com/watch?v=E2shqsYwxck
+Paper=https://arxiv.org/abs/2401.15884
+CodeRef:
+    - https://github.com/langchain-ai/langgraph/blob/main/examples/rag/langgraph_crag.ipynb
+    - https://github.com/langchain-ai/langgraph/blob/main/examples/rag/langgraph_crag_local.ipynb
+
+"""
