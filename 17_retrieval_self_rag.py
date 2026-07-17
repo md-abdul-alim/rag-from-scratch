0838874712f6721886d293e6433c8c4a7ce798e6
Md Abdul Alim <alim.abdul.5915@gmail.com>
2026-07-17 11:41:59 +0600

    Add initial implementation of 17_retrieval_self_rag.py with YouTube reference and code links

---
+++ 17_retrieval_self_rag.py
@@ -0,0 +1,10 @@
+
+
+"""
+Y.T.=https://www.youtube.com/watch?v=pbAd8O1Lvm4
+CodeRef:
+    - https://github.com/langchain-ai/langgraph/blob/main/examples/rag/langgraph_self_rag.ipynb
+    - https://github.com/langchain-ai/langgraph/blob/main/examples/rag/langgraph_self_rag_local.ipynb
+    - https://github.com/langchain-ai/langgraph/blob/main/examples/rag/langgraph_self_rag_pinecone_movies.ipynb
+
+"""
